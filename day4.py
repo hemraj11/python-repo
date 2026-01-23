98e3eb2db52434a48fc952b30d1ecd773298787f
hemraj11 <hemrajojha1122334455@gmail.com>
2026-01-23 15:19:48 +0545

    day4 assignment updated

---
+++ day4.py
@@ -0,0 +1,70 @@
+# loop--> for, while
+# loop is used to execute a statement multiple times
+
+# for loop
+# for i in range(1,6):
+#     print("Hello Hem", end=" ")
+#     # print(i, end=" ")
+    
+# for i in range(5):
+#     print(i, end=" ")
+
+# odd number 
+# for i in range(1, 11, 2):
+#     print(i, end=" ")
+
+# even number from 2 to 20
+# for i in range(2, 21, 2):
+#     print(i, end=" ")
+
+# string loop
+# name = "Python"
+# for i in name:
+#     print(i, end= " " )
+
+# str = "Hemraj"
+# for i in range(1, len(str)):
+#     print(str[i], end=" ")
+
+# # wap to check a number is prime or not
+# num = int(input("Enter a number: "))
+# factor = 0
+# for i in range(1, num+1):
+#     if num % i == 0:
+#        factor += 1
+#     if factor== 2:
+#         print(num, "is a prime number")
+#         break
+# else:
+#     print(num, "is not a prime number")
+
+
+
+# while loop
+# i = 1
+# while i<=5:
+#     print("Hello Hemraj")
+#     i += 1
+
+# Write a program to print even numbers from 1 to 100.
+# i = 2
+# while i<=20:
+#     print(i, end=" ")
+#     i += 2
+
+
+
+# Write a program to print odd numbers from 1 to 100.
+# i = 1
+# while i<=100:
+#         print(i, end=" ")
+#         i += 2
+
+# Write a program to find the sum of numbers from 1 to n.
+# n = int(input("Enter a number: "))
+# i = 1
+# sum = 0
+# while i<=n:
+#     sum += i
+#     i += 1
+# print(f"Sum of n natural numeber is {sum} ")
---
+++ day4Ass.py
@@ -0,0 +1,12 @@
+# print multiplication table of given number:
+# using while loop
+# num = int(input("Enter a number: "))
+# i =1 
+# while i<=10:
+#     print(f"{num} x {i} = {num*i}")
+#     i += 1
+
+# using for lopp
+num = int(input("Enter a number:"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
